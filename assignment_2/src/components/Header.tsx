669b0568bd24f4883ef7bbbb141d22fd2fc1d5a3
Chantelle Perreau <cperreau@myune.edu.au>
2022-08-12 01:03:13 +1000

    Button component and header styling added with placeholder button

--- assignment_2/src/components/Header.tsx
+++ assignment_2/src/components/Header.tsx
@@ -2,13 +2,17 @@ import React from 'react'
 import { useContext } from 'react'
 import { Link, useNavigate, useLocation } from 'react-router-dom'
 import style from './Header.module.css'
+import Button from './Button'
 
 
 
 function Header() {
     return (
         <div className={style.container}>
-            <header className={style.header}>Gomoku</header>
+            <header className={style.header}>
+                Gomoku <span className={style.author}>by Chantelle Perreau</span>
+            </header>
+            <Button>Login</Button>
         </div>
     )
 }
