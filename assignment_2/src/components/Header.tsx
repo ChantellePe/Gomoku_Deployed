72fd2a44938bdb01703b4a0f3b584d82573ca4da
Chantelle Perreau <cperreau@myune.edu.au>
2022-08-11 20:59:10 +1000

    Added react-router-dom and modified CSS of header

--- assignment_2/src/components/Header.tsx
+++ assignment_2/src/components/Header.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { useContext } from 'react'
+import { Link, useNavigate, useLocation } from 'react-router-dom'
 import style from './Header.module.css'
+
+
 
 function Header() {
     return (
-        <div>
+        <div className={style.container}>
             <header className={style.header}>Gomoku</header>
         </div>
     )
