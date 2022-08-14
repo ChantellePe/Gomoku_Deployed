1b0c1b2f7ec0c8ef91d151c03dbfc617770c536d
Chantelle Perreau <cperreau@myune.edu.au>
2022-08-15 02:55:12 +1000

    Additional Home Page styling

--- assignment_2/src/pages/Home.tsx
+++ assignment_2/src/pages/Home.tsx
@@ -19,10 +19,16 @@ export default function Home() {
 
                     }}>
                     <option value={0} hidden>Board Size</option>
-                    {(Array.from(Array(20)).map((e, i) => i + 5).map((num) =>
+                    {(Array.from(Array(15)).map((e, i) => i + 5).map((num) =>
                         <option value={num}>{num}</option>)
                     )}
                 </select>
+                <div className={style.gameRules}>
+                    <header className={style.rulesTitle}>Game Rules:</header>
+                    <p>Players alternate turns placing a stone of their color on an empty intersection. Black plays first.
+                        The winner is the first player to form an unbroken chain of five stones horizontally, vertically, or diagonally.
+                        Placing so that a line of more than five stones of the same color is created does not result in a win.</p>
+                </div>
                 <Button
                     className={style.startGameButton}
                     type="submit"
