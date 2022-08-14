68f7dca9c74ccc60a14b04ab4ce95737e21b476e
Chantelle Perreau <cperreau@myune.edu.au>
2022-08-14 20:35:14 +1000

    Create sign up button and protect Games and Gamelog pages

--- assignment_2/src/components/Header.tsx
+++ assignment_2/src/components/Header.tsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import { useContext } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
+import { Link, useNavigate, useLocation } from 'react-router-dom'
 import style from './Header.module.css'
 import buttonStyle from './Button.module.css'
 import '../index.css'
+import { useContext } from 'react'
 import { UserContext } from '../context'
 
 
 function Header() {
     const navigate = useNavigate()
     const { user } = useContext(UserContext)
+    const location = useLocation()
 
     const getActions = () => {
         if (user) {
@@ -17,9 +17,9 @@ function Header() {
                 <button className={buttonStyle.button} onClick={() => navigate('games')}>Previous Games</button>
             </>)
         } else {
-            return (
+            return location.pathname !== '/login' ? (
                 <button className={buttonStyle.button} onClick={() => navigate('login')}>Log In</button>
-            )
+            ) : <button className={buttonStyle.button}>Sign Up</button>
         }
     }
 
--- assignment_2/src/pages/GameLog.tsx
+++ assignment_2/src/pages/GameLog.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
 import { useParams } from 'react-router-dom'
+import { useContext } from 'react'
+import { Navigate } from 'react-router-dom'
+import { UserContext } from '../context'
+
 //import PreviousGameItem from '../components/PreviousGameItem';
 
 export default function GameLog() {
-
+    const { user } = useContext(UserContext)
     const { gameId } = useParams();
+    if (!user) return <Navigate to='/login' />
     if (!gameId) return null
 
 
