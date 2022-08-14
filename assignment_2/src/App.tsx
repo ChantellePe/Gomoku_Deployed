504564781971c098a4e344017ea9023be3a72b0c
Chantelle Perreau <cperreau@myune.edu.au>
2022-08-14 14:02:46 +1000

    Add Page Routes

--- assignment_2/src/App.tsx
+++ assignment_2/src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css';
 import { Header } from './components';
-import { Home, Login } from './pages';
+import { Home, Login, Game, GameLog, Games } from './pages';
 import { Routes, Route } from 'react-router-dom'
 
 function App() {
@@ -8,10 +8,15 @@ function App() {
     <>
       <Header />
       <main className='main'>
-        <Home />
-        {<Login />}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="game" element={<Game />} />
+          <Route path="games" element={<Games />} />
+          <Route path="gamelog/:id" element={<GameLog />} />
+          <Route path="*" element={<Home />} />
+        </Routes>
       </main>
-
     </>
   )
 }
